2c932c44ebf1798d3c3832a8cf84fad8c69ae26f
SmallPeenwow <bowdencamryn@gmail.com>
2022-12-21 17:39:22 +0200

    Order Of Operators

---
+++ OrderOperators.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    double x = 1 + 2 * 3;
+
+    cout << x;
+   
+    double x2 = (1 + 2) * 3;
+
+    cout << x2;
+
+    double x3 = 10;
+    double y = 5;
+    double z = (x3 + 10) / (3 * y);
+
+    cout << z;
+
+    return 0;
+}
