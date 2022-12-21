62327b3bdd6260ea120da09e48c02e6ebcfbb2c0
SmallPeenwow <bowdencamryn@gmail.com>
2022-12-21 20:29:41 +0200

    Using Maths Library

---
+++ WritingWithStandardLibrary.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    double result = floor(1.2);
+    cout << result;
+
+    double result2 = pow(2, 3);
+    cout << result2;
+
+    return 0;
+}
